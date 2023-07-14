dcf0f183aea5d8f94c39acd770668da6bff5d1c2
Mauricio Verano Merino <m.verano239@gmail.com>
2023-07-15 00:31:43 +0200

    move shapes2 to color

--- exercises/Shapes2/02-1.js
+++
@@ -1,7 +0,0 @@
-function setup() {
-      createCanvas(100,100);
-}
-
-function draw() {
-      background(255,255,0, 100);
-}
--- exercises/Shapes2/03-1.js
+++
@@ -1,8 +0,0 @@
-function setup() {
-      createCanvas(100,100);
-      background(255,255,0, 100);
-}
-
-function draw() {
-      
-}
