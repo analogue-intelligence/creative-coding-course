bbdeddfb00e6011b8ca759f1625118f156755b35
Mauricio Verano Merino <m.verano239@gmail.com>
2023-07-16 18:50:26 +0200

    added in class exercises about color transparency

---
+++ in_class_exercises/01_01_color_transparency/sketch.js
@@ -0,0 +1,19 @@
+function setup() {
+  createCanvas(400, 400);
+  
+  noStroke( );
+}
+
+function draw() {
+  background(255); 
+  fill(107, 197, 223);
+  rect(0,0,200,400);
+  fill(185, 82, 117,255);
+  rect(0,0,400,80);
+  fill(185, 82, 117,191);
+  rect(0,100,400,80);
+  fill(185, 82, 117,127);
+  rect(0,200,400,80);
+  fill(185, 82, 117,63);
+  rect(0,300,400,80);
+}
