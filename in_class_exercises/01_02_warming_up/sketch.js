bbe639668628dd0e58292e73a88718b155caeece
Mauricio Verano Merino <m.verano239@gmail.com>
2023-07-17 16:13:57 +0200

    added project for a warming up exercises

---
+++ in_class_exercises/01_02_warming_up/sketch.js
@@ -0,0 +1,17 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(0);
+  noStroke();
+
+  fill(240);
+  rect(40,60,320,320);
+
+  fill(200);
+  rect(80,120,240,240);
+
+  fill(130);
+  rect(120,180,160,160);
+}
