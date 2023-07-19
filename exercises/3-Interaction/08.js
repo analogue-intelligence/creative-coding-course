5ac1da34bcd5a3bdf8c05bee9150f80ae854a4d9
Nicolas F. Chaves-de-Plaza <chadepl@Nicolas-TUD-MacBook-Pro.local>
2023-07-19 09:53:16 +0200

    Removed some extra help from the exercise

--- exercises/3-Interaction/08.js
+++ exercises/3-Interaction/08.js
@@ -1,26 +1,20 @@
 // Draw using the mouse, and also work with mousePressed() and keyPressed() events
-    
-  // Every time the mouse is pressed a circle is drawn on the coordinates
-  function mousePressed(){
-    //Inverting black and white for a better visual
-    stroke(255);
-    fill(0);
 
-    //Draw ellipse here
-  }
+function setup() {
 
- // When a key is pressed the background becomes white again - refreshing the screen
-  function keyPressed(){
+  createCanvas(100, 100);
+  background(0); // Background is specified here so as to be able to draw on canvas
 
-  }
-  
-  function setup() {
+}
 
-    createCanvas(100, 100);
-    background(0); // Background is specified here so as to be able to draw on canvas
-    
-  }
-  
-  function draw() {
-    
-  }
+function draw() {
+
+}
+
+// Implement mousePressed function. This function will be called automatically everytime the user pressed the mouse's left button
+// Check for more details: https://p5js.org/reference/#/p5/mousePressed
+// Every time the mouse is pressed a circle is drawn on the current mouse coordinates
+
+// Implement keyPressed function. This function will be called automatically everytime the user presses a keyboard key
+// Check for more details: https://p5js.org/reference/#/p5/keyPressed
+// When a key is pressed the background becomes white again - refreshing the screen
